a5a4a909fdc5fa12ceabf33e439e7758eeddda1b
mayukhasripathi <68654861+mayukhasripathi@users.noreply.github.com>
2020-07-22 11:49:10 -0400

    First Xcode project.

---
+++ StoryboardIntro/StoryboardIntro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryboardIntro
+//
+//  Created by Maya on 7/22/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello World!")
+    }
+
+
+}
+
