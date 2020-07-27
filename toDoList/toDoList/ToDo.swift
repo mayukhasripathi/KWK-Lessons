5de444c266eba417b14a4daebf439b2c757d348c
mayukhasripathi <68654861+mayukhasripathi@users.noreply.github.com>
2020-07-27 14:21:34 -0400

    Complete Iteration 0

---
+++ toDoList/toDoList/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  toDoList
+//
+//  Created by Maya on 7/27/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ToDoClass {
+    var description = ""
+    var important = false
+}
+
